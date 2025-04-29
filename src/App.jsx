217247a222248d706e4af8d222b79f77da6a3376
sujalkakatkar07 <sujalkakatkar54@gmail.com>
2025-04-29 07:18:13 +0530

    created appwrite service and redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userDate) => {
+        if (userDate) {
+          dispatch(login({userDate}))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(()=>setLoading(false))
+    
+  },[])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return !loading ? (
+    <div className='w-full block'>
+        <div className='min-h-screen flex flex-wrap flex-col content-between justify-between bg-gray-400'>
+        <Header />
+        <main>
+          todos:
+          {/* outlet */}
+        </main>
+        <Footer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  ):null
 }
 
 export default App
