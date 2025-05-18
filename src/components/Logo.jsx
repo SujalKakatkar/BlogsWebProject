e719292ab0241c166fb0c69fcd135b8c975aded4
sujalkakatkar07 <sujalkakatkar54@gmail.com>
2025-05-18 11:36:38 +0530

    logo fixing

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../assests/images/logo.png'
 
 function Logo({width="100px"}) {
   return (
     <div className='text-white'>
-      <img src="/images/logo.png" alt="megablog" className='w-12 rounded-full' />
+      <img src={logo} alt="megablog" className='w-12 rounded-full' />
     </div>
   )
 }
