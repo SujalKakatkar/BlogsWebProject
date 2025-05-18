ecb88cd7803fa08f2a68dac66baaa5776ff71fd6
sujalkakatkar07 <sujalkakatkar54@gmail.com>
2025-05-18 11:11:36 +0530

    logo file fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width="100px"}) {
   return (
     <div className='text-white'>
-      <img src="/public/Images/ChatGPT Image May 14, 2025, 07_10_44 AM.png" alt="megablog" className='w-12 rounded-full' />
+      <img src="/public/Images/logo.png" alt="megablog" className='w-12 rounded-full' />
     </div>
   )
 }
